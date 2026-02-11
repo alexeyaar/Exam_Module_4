095c383800fabb5b849dd8eab9c88cff041d431a
Alexey Artemov <alexey.rk.tinkoff@gmail.com>
2026-02-11 12:13:40 +0300

    exam commit2

--- Cinescope/tests/api/test_auth_api_negative.py
+++ Cinescope/tests/api/test_auth_api_negative.py
@@ -1,28 +1,28 @@
 import pytest
+
+from Cinescope.clients.api_manager import ApiManager
 from Cinescope.constants import LOGIN_ENDPOINT
 
 
 @pytest.mark.negative
 class TestNegativeAuth:
-    def test_auth_no_password(self,test_user,requester):
+    def test_auth_no_password(self,test_user,requester,api_manager: ApiManager):
      """Авторизация без пароля"""
-
+     api_manager.auth_api.register_user(test_user)
      login_data = {
          "email":test_user["email"],
          "password": ""
      }
-     response_auth = requester.send_request(
-     method="POST",
-     endpoint=LOGIN_ENDPOINT,
-     data=login_data,
-     expected_status=401
-     )
+     response = api_manager.auth_api.login_user(login_data,expected_status=401)
+     assert "message" in response.json(), "В теле ответа отсутствует сообщение об ошибке"
+     assert "error" in response.json(), "В теле ответа отсутствует описание ошибки"
+     assert "statusCode" in response.json(), "В теле ответа отсутствует статус код"
 
-    def test_auth_no_body(self,test_user,requester):
+    def test_auth_no_body(self,test_user,requester,api_manager):
      """Авторизация без тела запроса """
-     response_2auth = requester.send_request(
-     method="POST",
-     endpoint=LOGIN_ENDPOINT,
-     data="",
-      expected_status=400
-     )
+     api_manager.auth_api.register_user(test_user)
+     login_data = { }
+     response = api_manager.auth_api.login_user(login_data, expected_status=401)
+     assert "message" in response.json(), "В теле ответа отсутствует сообщение об ошибке"
+     assert "error" in response.json(), "В теле ответа отсутствует описание ошибки"
+     assert "statusCode" in response.json(), "В теле ответа отсутствует статус код"
